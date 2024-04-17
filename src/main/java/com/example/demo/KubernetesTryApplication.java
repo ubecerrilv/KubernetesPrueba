1015286eeeeaa5e13ec05f6a415853b3d36bb740
ulises-desktop <ulisesbecerril81@gmail.com>
2024-04-16 22:31:38 -0600

    Commit inicial

---
+++ src/main/java/com/example/demo/KubernetesTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesTryApplication.class, args);
+	}
+
+}
